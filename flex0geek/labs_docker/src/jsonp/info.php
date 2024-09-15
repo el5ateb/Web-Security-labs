4a6b67463540f056ec096c94eb205e0af5962263
Mahmoud Elkhateb <Mahmoud.Elkhateb@proton.me>
2024-09-15 16:26:45 +0300

    Delete flex0geek/labs_docker directory

--- flex0geek/labs_docker/src/jsonp/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header("Content-Type: application/json");
-if(isset($_GET['callback'])){
-	echo $_GET['callback'].'({"username":"guest","Full Name":"Guest user", "email":"guest@gmail.com","privateGroups":["priv","private","our course","Work Group"]})';
-}else{
-	echo '{"username":"guest","Full Name":"Guest user", "email":"guest@gmail.com","privateGroups":["priv","private","our course","Work Group"]}';
-}
